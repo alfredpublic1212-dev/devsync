a8ee9ed4ab05be3e1323f745a2e2e0a1cd0cd0a8
Deep <deepanrajpmk@gmail.com>
2026-01-21 16:53:28 +0530

    fix:oicd redirect url change

--- app/provider/OidcProvider.tsx
+++ app/provider/OidcProvider.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from 'react-oidc-context';
 const oidcConfig = {
   authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_QZ4kjF1Xq',
   client_id: '4cs8n063hjssf8s6tlj2qeogbc',
-  redirect_uri: 'http://localhost:3000',
+  redirect_uri: 'https://devsync-teal.vercel.app/',
   response_type: 'code',
   scope: 'openid email phone',
 };
