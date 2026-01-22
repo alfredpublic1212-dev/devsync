738b5dd4a750923c4279c9dffc834b4c9c48995b
Deep <deepanrajpmk@gmail.com>
2026-01-22 21:38:17 +0530

    feat:api route for auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import type { JWT } from "next-auth/jwt";
+import type { Session, User } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({
+      token,
+      user,
+    }: {
+      token: JWT;
+      user?: User;
+    }): Promise<JWT> {
+      if (user) {
+        token.userId = user.id;
+      }
+      return token;
+    },
+
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }): Promise<Session> {
+      if (session.user) {
+        session.user.id = token.userId as string;
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
